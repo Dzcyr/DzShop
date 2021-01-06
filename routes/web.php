7a8c65cceef91dc46aa0423a412bb38e42656664
冬字差一人 <329636491@qq.com>
2021-01-06 17:25:55 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Auth::routes(['verify' => true]);
 Route::redirect('/', '/products')->name('root');
 // 商品管理
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     // 用户收货地址管理
