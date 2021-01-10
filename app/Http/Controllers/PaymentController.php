f7a9228f5eff8313b7f2a34a26a4f4cfcb70c7bc
冬字差一人 <329636491@qq.com>
2021-01-10 13:47:49 +0800

    微信支付

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Order;
+use Endroid\QrCode\QrCode;
 use Illuminate\Http\Request;
 use App\Exceptions\InvalidRequestException;
 
 class PaymentController extends Controller
 {
+    // 支付宝
     public function payByAlipay(Order $order, Request $request)
     {
         // 判断订单是否属于当前用户
@@ -19,13 +21,37 @@ class PaymentController extends Controller
         }
         // 调用支付宝的网页支付
         return app('alipay')->web([
-            // 订单编号，需保证在商户端不重复
+            // 商户订单流水号
             'out_trade_no' => $order->no,
             // 订单金额，单位元，支持小数点后两位
             'total_amount' => $order->total_amount,
             // 订单标题
-            'subject'      => '支付 Dz Shop 的订单：'.$order->no,
+            'subject' => '支付订单：'.$order->no,
         ]);
+    }
+
+    // 微信
+    public function payByWechat(Order $order, Request $request)
+    {
+        // 判断订单是否属于当前用户
+        $this->authorize('own', $order);
+        // 订单已支付或者已关闭
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // scan 方法为拉起微信扫码支付
+        $wechatOrder = app('wechat_pay')->scan([
+            // 商户订单流水号
+            'out_trade_no' => $order->no,
+            // 订单金额，单位分
+            'total_fee' => $order->total_amount * 100,
+            // 订单标题
+            'body' => '支付订单：'.$order->no,
+        ]);
+        // 把要转换的字符串作为 QrCode 的构造函数参数
+        $qrCode = new QrCode($wechatOrder->code_url);
+        // 将生成的二维码图片数据以字符串形式输出，并带上相应的响应类型
+        return response($qrCode->writeString(), 200, ['Content-Type' => $qrCode->getContentType()]);
     }
 
     // 前端回调页面
@@ -40,7 +66,7 @@ class PaymentController extends Controller
         return view('pages.success', ['msg' => '付款成功']);
     }
 
-    // 服务器端回调
+    // 支付宝 服务器端回调
     public function alipayNotify()
     {
         // 校验输入参数
@@ -61,11 +87,41 @@ class PaymentController extends Controller
         }
 
         $order->update([
-            'paid_at'        => Carbon::now(), // 支付时间
-            'payment_method' => 'alipay', // 支付方式
-            'payment_no'     => $data->trade_no, // 支付宝订单号
+            // 支付时间
+            'paid_at' => Carbon::now(),
+            // 支付方式
+            'payment_method' => 'alipay',
+            // 支付宝订单号
+            'payment_no' => $data->trade_no,
         ]);
 
         return app('alipay')->success();
+    }
+
+    // 微信 服务器端回调
+    public function wechatNotify()
+    {
+        // 校验回调参数是否正确
+        $data  = app('wechat_pay')->verify();
+        // 找到对应的订单
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 订单不存在则告知微信支付
+        if (!$order) {
+            return 'fail';
+        }
+        // 订单已支付
+        if ($order->paid_at) {
+            // 告知微信支付此订单已处理
+            return app('wechat_pay')->success();
+        }
+
+        // 将订单标记为已支付
+        $order->update([
+            'paid_at'        => Carbon::now(),
+            'payment_method' => 'wechat',
+            'payment_no'     => $data->transaction_id,
+        ]);
+
+        return app('wechat_pay')->success();
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = 'route('payment.alipay.notify')';
+            $config['notify_url'] = route('payment.alipay.notify');
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
@@ -29,6 +29,20 @@ class AppServiceProvider extends ServiceProvider
             }
             // 调用 Yansongda\Pay 来创建一个支付宝支付对象
             return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            $config['notify_url'] = route('payment.wechat.notify');
+            $config['return_url'] = route('payment.wechat.return');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            return Pay::wechat($config);
         });
     }
 
